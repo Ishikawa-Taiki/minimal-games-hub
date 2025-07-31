93333bce59ba8db369affa4ef043338c89ae6db1
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-07-31 22:52:26 +0900

    fix: GitHub Pagesでのリンク切れを修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'minimal-games-hub';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   /* config options here */
 };
 
