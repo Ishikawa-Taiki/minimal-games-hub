f03c51f83053f8dcc8959fc69d43800c9a6d0396
Ishikawa-Taiki <ishikawataiki.0815@gmail.com>
2025-08-07 00:10:02 +0900

    feat: Github Codespacesのサポートを追加

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Check if nodenv is installed
-if ! command -v nodenv &> /dev/null
-then
-    echo "nodenv could not be found. Please install it."
-    exit 1
-fi
-
-# Install the correct node version
-nodenv install
-
 # Install dependencies
 npm install
+
+echo "Setup complete!"
